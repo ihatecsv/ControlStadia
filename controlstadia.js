c28aabe0735c4e94b0f352bc28bbce736a5c4cea
Drake Luce <me@drakeluce.com>
2020-03-05 12:30:38 -0400

    Blindly apply standard mapping... Stadia responds!

--- controlstadia.js
+++ controlstadia.js
@@ -5,11 +5,31 @@ function main(){
 		setupCS();
 	}, false);
 	const setupCS = function(){
-		console.log("ControlStadia: Injected!")
+		console.log("ControlStadia: Injected!");
+		const originalGetGamepads = navigator.getGamepads;
+		navigator.getGamepads = function(){ // The magic happens here
+			const originalGamepads = originalGetGamepads.apply(navigator);
+			const modifiedGamepads = [originalGamepads[0], null, null, null];
+			let insertIndex = 1;
+			for(let i = 0; i < 4; i++){
+				if(insertIndex >= 4) break;
+				if(originalGamepads[i] !== null){
+					modifiedGamepads[insertIndex] = {};
+					for(let property in originalGamepads[i]){
+						modifiedGamepads[insertIndex][property] = originalGamepads[i][property];
+					}
+					modifiedGamepads[insertIndex].index = insertIndex;
+					modifiedGamepads[insertIndex].mapping = "standard";
+					insertIndex++;
+				} 
+			}
+			return modifiedGamepads;
+		}
 	}
 }
 
 chrome.storage.sync.get([
+	""
 ], function(settings) {
 	settings.extUrl = chrome.runtime.getURL("/");
 	if(settings.disableTouchStadia) return;
