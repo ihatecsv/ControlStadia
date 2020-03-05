bb9acb2cbdaea9e95ca7a4b3e42153e4c7be15bd
Drake Luce <me@drakeluce.com>
2020-03-05 15:57:21 -0400

    Control mapping working, not pretty though!

--- controlstadia.js
+++ controlstadia.js
@@ -6,30 +6,75 @@ function main(){
 	}, false);
 	const setupCS = function(){
 		console.log("ControlStadia: Injected!");
+		const emulatedGamepad = {
+			id: "ControlStadia emulated gamepad",
+			index: 0,
+			connected: true,
+			timestamp: 0,
+			mapping: "standard",
+			axes: [0, 0, 0, 0],
+			buttons: []
+		}
 		const originalGetGamepads = navigator.getGamepads;
 		navigator.getGamepads = function(){ // The magic happens here
 			const originalGamepads = originalGetGamepads.apply(navigator);
-			const modifiedGamepads = [originalGamepads[0], null, null, null];
-			let insertIndex = 1;
-			for(let i = 0; i < 4; i++){
-				if(insertIndex >= 4) break;
-				if(originalGamepads[i] !== null){
-					modifiedGamepads[insertIndex] = {};
-					for(let property in originalGamepads[i]){
-						modifiedGamepads[insertIndex][property] = originalGamepads[i][property];
+			for(let i = 0; i < 17; i++){
+				if(typeof config.buttons[i].dstID !== "undefined"){
+					let found = false;
+					let gamepadID;
+					for(gamepadID = 0; gamepadID < originalGamepads.length; gamepadID++){
+						if(originalGamepads[gamepadID] !== null && originalGamepads[gamepadID].id === config.buttons[i].dstID){
+							found = true;
+							break;
+						}
 					}
-					modifiedGamepads[insertIndex].index = insertIndex;
-					modifiedGamepads[insertIndex].mapping = "standard";
-					insertIndex++;
-				} 
+					if(found){
+						if(config.buttons[i].dstType){ //Axis
+							emulatedGamepad.buttons[i] = {
+								"pressed": originalGamepads[gamepadID].axes[config.buttons[i].dstIndex] !== 0,
+								"touched": originalGamepads[gamepadID].axes[config.buttons[i].dstIndex] !== 0,
+								"value": originalGamepads[gamepadID].axes[config.buttons[i].dstIndex]
+							}
+						}else{ //Button
+							emulatedGamepad.buttons[i] = originalGamepads[gamepadID].buttons[config.buttons[i].dstIndex];
+						}
+						emulatedGamepad.timestamp = originalGamepads[gamepadID].timestamp;
+					}else{
+						emulatedGamepad.buttons[i] = {pressed: false, touched: false, value: 0};
+					}
+				}else{
+					emulatedGamepad.buttons[i] = {pressed: false, touched: false, value: 0};
+				}
 			}
-			return modifiedGamepads;
+
+			for(let i = 0; i < 4; i++){
+				if(typeof config.axes[i].dstID !== "undefined"){
+					let found = false;
+					let gamepadID;
+					for(gamepadID = 0; gamepadID < originalGamepads.length; gamepadID++){
+						if(originalGamepads[gamepadID] !== null && originalGamepads[gamepadID].id === config.axes[i].dstID){
+							found = true;
+							break;
+						}
+					}
+					if(found){
+						emulatedGamepad.axes[i] = originalGamepads[gamepadID].axes[config.axes[i].dstIndex];
+						emulatedGamepad.timestamp = originalGamepads[gamepadID].timestamp;
+					}else{
+						emulatedGamepad.axes[i] = 0;
+					}
+				}else{
+					emulatedGamepad.axes[i] = 0;
+				}
+			}
+			return [emulatedGamepad, null, null, null];
 		}
 	}
 }
 
 chrome.storage.sync.get([
-	""
+	"axes",
+	"buttons"
 ], function(settings) {
 	settings.extUrl = chrome.runtime.getURL("/");
 	if(settings.disableTouchStadia) return;
--- settings.js
+++ settings.js
@@ -1,12 +1,127 @@
 document.getElementById("version").innerText = chrome.runtime.getManifest().version;
 
+const firstRunNotificationElem = document.getElementById("first-run-notification");
+const firstRunNotificationCloseButtonElem = document.getElementById("first-run-notification-close-button");
+const settingsElem = document.getElementById("settings");
+const applyButtonElem = document.getElementById("apply-button");
+const gamepadRefreshButtonElem = document.getElementById("gamepad-refresh-button");
+const gamepadSelectElem = document.getElementById("gamepad-select");
+const buttonListElem = document.getElementById("button-list");
+
+const axes = [
+    {label: "LX"}, //0
+    {label: "LY"}, //1
+    {label: "RX"}, //2
+    {label: "RY"}, //3
+]
+
+const buttons = [
+    {label: "A"}, //0
+    {label: "B"}, //1
+    {label: "X"}, //2
+    {label: "Y"}, //3
+    {label: "L1"}, //4
+    {label: "R1"}, //5
+    {label: "L2"}, //6
+    {label: "R2"}, //7
+    {label: "Select"}, //8
+    {label: "Start"}, //9
+    {label: "L3"}, //10
+    {label: "R3"}, //11
+    {label: "Up"}, //12
+    {label: "Down"}, //13
+    {label: "Left"}, //14
+    {label: "Right"}, //15
+    {label: "Home"} //16
+]
+
+const populateGamepadSelect = function(){
+    gamepadSelectElem.innerHTML = "";
+    const gamepads = navigator.getGamepads();
+    for(let i = 0; i < gamepads.length; i++){
+        if(gamepads[i] !== null){
+            const optionElem = document.createElement("option");
+            optionElem.value = gamepads[i].index;
+            optionElem.innerText = gamepads[i].id;
+            gamepadSelectElem.add(optionElem);
+        }
+    }
+    console.log("ControlStadia: Refreshed gamepad list!");
+}
+
+const awaitInputToSet = function(srcType, srcIndex, buttonLabelElem){
+    const gamepadSelectedIndex = parseInt(gamepadSelectElem.value);
+    const originalGamepadState = navigator.getGamepads()[gamepadSelectedIndex];
+    const originalButtonState = originalGamepadState.buttons.slice();
+    const originalAxesState = originalGamepadState.axes.slice();
+    const changeFound = function(dstType, dstIndex){
+        buttonLabelElem.innerHTML = (dstType ? "Axis" : "Button") + " " + dstIndex;
+        (srcType ? axes : buttons)[srcIndex].dstIndex = dstIndex;
+        (srcType ? axes : buttons)[srcIndex].dstType = dstType;
+        (srcType ? axes : buttons)[srcIndex].dstID = originalGamepadState.id;
+    }
+    const checkInterval = setInterval(function(){
+        const currentGamepadState = navigator.getGamepads()[gamepadSelectedIndex];
+        const currentButtonState = currentGamepadState.buttons.slice();
+        const currentAxesState = currentGamepadState.axes.slice();
+        for(let i = 0; i < originalButtonState.length; i++){
+            if(
+                currentButtonState[i].pressed !== originalButtonState[i].pressed ||
+                currentButtonState[i].touched !== originalButtonState[i].touched ||
+                currentButtonState[i].value !== originalButtonState[i].value
+            ){
+                changeFound(0, i);
+                clearInterval(checkInterval);
+                break;
+            }
+        }
+        for(let i = 0; i < originalAxesState.length; i++){
+            if(currentAxesState[i] !== originalAxesState[i]){
+                changeFound(1, i);
+                clearInterval(checkInterval);
+                break;
+            }
+        }
+    }, 200);
+}
+
+const populateButtonList = function(){
+    buttonListElem.innerHTML = "";
+    for(let i = 0; i < buttons.length; i++){
+        const buttonButtonElem = document.createElement("button");
+        buttonButtonElem.innerHTML = buttons[i].label;
+        const buttonLabelElem = document.createElement("span");
+        buttonLabelElem.innerHTML = "[Unset]";
+        const breakElem = document.createElement("br");
+        buttonListElem.appendChild(buttonButtonElem);
+        buttonListElem.appendChild(buttonLabelElem);
+        buttonListElem.appendChild(breakElem);
+        buttonButtonElem.onclick = function(){
+            buttonLabelElem.innerHTML = "[Press button/move axis]";
+            awaitInputToSet(0, i, buttonLabelElem);
+        }
+    }
+    for(let i = 0; i < axes.length; i++){
+        const buttonButtonElem = document.createElement("button");
+        buttonButtonElem.innerHTML = axes[i].label;
+        const buttonLabelElem = document.createElement("span");
+        buttonLabelElem.innerHTML = "[Unset]";
+        const breakElem = document.createElement("br");
+        buttonListElem.appendChild(buttonButtonElem);
+        buttonListElem.appendChild(buttonLabelElem);
+        buttonListElem.appendChild(breakElem);
+        buttonButtonElem.onclick = function(){
+            buttonLabelElem.innerHTML = "[Press button/move axis]";
+            awaitInputToSet(1, i, buttonLabelElem);
+        }
+    }
+    console.log("ControlStadia: Populated button list!");
+}
+populateButtonList();
+
 chrome.storage.sync.get([
     "firstRun"
 ], function(settings) {
-    const firstRunNotificationElem = document.getElementById("first-run-notification");
-    const firstRunNotificationCloseButtonElem = document.getElementById("first-run-notification-close-button");
-    const settingsElem = document.getElementById("settings");
-    const applyButtonElem = document.getElementById("apply-button");
 
     if(settings.firstRun){
         settingsElem.style.display = "none";
@@ -23,10 +138,17 @@ chrome.storage.sync.get([
     }
 
     applyButtonElem.onclick = function(){
-        const startParams = {};
-        chrome.storage.sync.set(startParams, function(){
+        const options = {
+            axes: axes,
+            buttons: buttons
+        };
+        chrome.storage.sync.set(options, function(){
             console.log("ControlStadia: Set options!");
             chrome.tabs.reload();
         });
+    }
+
+    gamepadRefreshButtonElem.onclick = function(){
+        populateGamepadSelect();
     }
 });
